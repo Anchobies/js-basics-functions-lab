bbca64ee52d24762c76e248c3321999ded241285
Anchobies <anthonychung97@gmail.com>
2021-05-17 18:11:58 -0400

    commit tree a1433277c7d036a94d9109868775cf9a4920586c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = location => Math.abs(location - 42);
+
+const distanceFromHqInFeet = location => distanceFromHqInBlocks(location) * 264;
+
+const distanceTravelledInFeet = (start, end) => Math.abs(start - end) * 264;
+
+const calculatesFarePrice = (start, end) => {
+    const distance = distanceTravelledInFeet(start, end);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        return 0.02 * (distance - 400);
+    }
+}
